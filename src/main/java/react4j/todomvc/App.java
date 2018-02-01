519e7d642ca6d774bc46f6a9ca5f4216c2111d56
Peter Donald <peter@realityforge.org>
2018-02-02 08:59:12 +1100

    Make sure Arez components are created at the top level

--- src/main/java/react4j/todomvc/App.java
+++ src/main/java/react4j/todomvc/App.java
@@ -4,6 +4,8 @@ import com.google.gwt.core.client.EntryPoint;
 import elemental2.dom.DomGlobal;
 import react4j.arez.spy.ArezSpyUtil;
 import react4j.dom.ReactDOM;
+import react4j.todomvc.model.AppData;
+import react4j.todomvc.model.ViewService;
 
 public class App
   implements EntryPoint
@@ -12,6 +14,10 @@ public class App
   public void onModuleLoad()
   {
     ArezSpyUtil.enableSpyEventLogging();
+    // This next line forces the creation of all the resources, ensuring that the transactions
+    // are not wrapped in another transaction
+    @SuppressWarnings( "unused" )
+    final ViewService viewService = AppData.viewService;
     ReactDOM.render( TodoList.create(), DomGlobal.document.getElementById( "todoapp" ) );
   }
 }
