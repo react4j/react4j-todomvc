325ca3ec7953a0576933a4cd2d94d63d85f7fe84
Peter Donald <peter@realityforge.org>
2018-02-21 10:16:53 +1100

    Use local subscription model to decide when to re-render

--- src/main/java/react4j/todomvc/App.java
+++ src/main/java/react4j/todomvc/App.java
@@ -4,7 +4,6 @@ import com.google.gwt.core.client.EntryPoint;
 import elemental2.dom.DomGlobal;
 import react4j.dom.ReactDOM;
 import react4j.todomvc.model.AppData;
-import react4j.todomvc.model.ViewService;
 
 public class App
   implements EntryPoint
@@ -12,9 +11,12 @@ public class App
   @Override
   public void onModuleLoad()
   {
-    // This next line forces the creation of all the resources
-    @SuppressWarnings( "unused" )
-    final ViewService viewService = AppData.viewService;
+    AppData.service.subscribe( this::render );
+    render();
+  }
+
+  private void render()
+  {
     ReactDOM.render( TodoListBuilder.build(), DomGlobal.document.getElementById( "todoapp" ) );
   }
 }
