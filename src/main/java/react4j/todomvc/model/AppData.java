5ebb0893e610033f4c67d8a37d240fa1b952516f
Peter Donald <peter@realityforge.org>
2018-02-21 09:32:46 +1100

    Move to non-arez state management

--- src/main/java/react4j/todomvc/model/AppData.java
+++ src/main/java/react4j/todomvc/model/AppData.java
@@ -4,9 +4,8 @@ import arez.browser.extras.BrowserLocation;
 
 public final class AppData
 {
+  public static final TodoRepository service = new TodoRepository();
   public static final BrowserLocation location = BrowserLocation.create();
-  public static final TodoRepository model = TodoRepository.newRepository();
-  public static final TodoService service = new Arez_TodoService( model );
   public static final ViewService viewService = new Arez_ViewService( model, location );
 
   private AppData()
