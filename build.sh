a0427279bffc891a8c3d76624f6fdcd12b50c746
Peter Donald <peter@realityforge.org>
2018-06-24 19:56:30 +1000

    Move tmp diretory locally so it easier to clear

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
-rm -rf jsZipCache out/sources out/app.js
-mvn clean package -Pdevmode
+rm -rf jsZipCache out/sources out/app.js tmp/j2cl
+mkdir -p tmp/j2cl
+mvn clean package -Pdevmode -Djava.io.tmpdir=tmp/j2cl
