dfe80477c5f1de2d9be4d436c62184f7cec68f23
Peter Donald <peter@realityforge.org>
2018-06-24 21:21:27 +1000

    Prepare for next version of react4j

--- src/main/java/react4j/todomvc/FooterTodoCount.java
+++ src/main/java/react4j/todomvc/FooterTodoCount.java
@@ -4,7 +4,7 @@ import javax.annotation.Nullable;
 import javax.inject.Inject;
 import react4j.annotations.ReactComponent;
 import react4j.arez.ReactArezComponent;
-import react4j.core.ReactNode;
+import react4j.ReactNode;
 import react4j.dom.proptypes.html.HtmlProps;
 import react4j.todomvc.model.TodoRepository;
 import static react4j.dom.DOM.*;
