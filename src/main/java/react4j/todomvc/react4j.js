8559a46848bf7ad03ba574f081d0e3bcbbe6d545
Peter Donald <peter@realityforge.org>
2018-07-01 21:29:14 +1000

    Add some required defines

---
+++ src/main/java/react4j/todomvc/react4j.js
@@ -0,0 +1,32 @@
+/*
+ * If you change this file then run
+ * cd src/main/java && zip react.js.zip react4j/todomvc/react4j.js
+ */
+
+goog.provide('react4j.todomvc.react4j');
+
+/** @define {string} */
+goog.define('react4j.environment', 'production');
+/** @define {string} */
+goog.define('react4j.enable_component_names', 'false');
+/** @define {string} */
+goog.define('react4j.check_component_state_invariants', 'false');
+/** @define {string} */
+goog.define('react4j.check_invariants', 'false');
+
+goog.provide('react4j.arez');
+
+/** @define {string} */
+goog.define('react4j.arez.store_arez_data_as_state', 'false');
+
+goog.provide('braincheck');
+
+/** @define {string} */
+goog.define('braincheck.environment', 'production');
+
+/** @define {string} */
+goog.define('braincheck.verbose_error_messages', 'false');
+/** @define {string} */
+goog.define('braincheck.check_invariants', 'false');
+/** @define {string} */
+goog.define('braincheck.check_api_invariants', 'false');
