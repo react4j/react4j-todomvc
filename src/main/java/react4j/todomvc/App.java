63f6e0505b83991afb73f58f716fa6cc7f361a3a
Peter Donald <peter@realityforge.org>
2018-12-21 20:49:51 +1100

    Remove variables

--- src/main/java/react4j/todomvc/App.java
+++ src/main/java/react4j/todomvc/App.java
@@ -9,8 +9,6 @@ import react4j.dom.ReactDOM;
 import react4j.todomvc.dagger.OtherComponent;
 import react4j.todomvc.dagger.OtherComponent2;
 import react4j.todomvc.dagger.TodoComponent;
-import react4j.todomvc.dagger.foo.Foo;
-import react4j.todomvc.dagger.foo2.Enhanced_Foo2;
 
 public class App
   implements EntryPoint
@@ -24,8 +22,8 @@ public class App
       ReactArezSpyUtil.enableSpyEventLogging();
     }
     TodoComponent.create();
-    final Foo foo = OtherComponent.create().getFooDaggerSubcomponent().createProvider().get();
-    final Enhanced_Foo2 foo2 = OtherComponent2.create().createEnhanced_Foo2Provider().get();
+    OtherComponent.create().getFooDaggerSubcomponent().createProvider().get();
+    OtherComponent2.create().createEnhanced_Foo2Provider().get();
     Js.debugger();
     ReactDOM.render( TodoListBuilder.build(), DomGlobal.document.getElementById( "todoapp" ) );
   }
