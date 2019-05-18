10c0292d9dc6176a0bb2b19f90cb8f6dd9126f27
Peter Donald <peter@realityforge.org>
2019-05-19 07:39:52 +1000

    Move to a uniquely named module for entrypoint

--- src/main/java/react4j/todomvc/Main.js
+++ src/main/java/react4j/todomvc/Main.js
@@ -1,6 +1,6 @@
-goog.module('react4j.todomvc');
+goog.module('react4j.todomvc.Main');
 
 // Here we use goog.require to import the Java class to this module.
 const App = goog.require('react4j.todomvc.App');
 
-App.onModuleLoad();
+App.$create__().m_onModuleLoad__();
