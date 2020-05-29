83b006b74c91672dc91ac4ba63e58d37e649155e
Peter Donald <peter@realityforge.org>
2020-05-29 11:00:37 +1000

    Add nullability annotations

--- src/main/java/react4j/todomvc/model/AppData.java
+++ src/main/java/react4j/todomvc/model/AppData.java
@@ -1,8 +1,12 @@
 package react4j.todomvc.model;
 
+import javax.annotation.Nonnull;
+
 public final class AppData
 {
+  @Nonnull
   public static final TodoRepository service = new TodoRepository();
+  @Nonnull
   public static final ViewService viewService = new ViewService( service );
 
   private AppData()
