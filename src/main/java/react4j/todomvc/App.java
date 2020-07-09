a3d1b0483301e367ae9479827f0d11e86ad93fe0
Peter Donald <peter@realityforge.org>
2020-07-09 15:54:26 +1000

    Remove unnecessary wrapping

--- src/main/java/react4j/todomvc/App.java
+++ src/main/java/react4j/todomvc/App.java
@@ -3,7 +3,6 @@ package react4j.todomvc;
 import arez.spytools.browser.react4j.ReactArezSpyUtil;
 import com.google.gwt.core.client.EntryPoint;
 import elemental2.dom.DomGlobal;
-import react4j.ReactElement;
 import react4j.dom.ReactDOM;
 
 public final class App
@@ -13,7 +12,6 @@ public final class App
   public void onModuleLoad()
   {
     ReactArezSpyUtil.enableSpyEventLogging();
-    ReactDOM.render( ReactElement.createStrictMode( TodoListBuilder.build() ),
-                     DomGlobal.document.getElementById( "app" ) );
+    ReactDOM.render( TodoListBuilder.build(), DomGlobal.document.getElementById( "app" ) );
   }
 }
