235df9a39c6cb188ff00987a3d634a124e82167c
Peter Donald <peter@realityforge.org>
2026-04-15 17:12:22 +1000

    Update to the latest react4j API

--- src/main/java/react4j/todomvc/TodoItem.java
+++ src/main/java/react4j/todomvc/TodoItem.java
@@ -33,7 +33,7 @@ abstract class TodoItem
   private boolean _isEditing;
   private String _editText;
 
-  TodoItem( @Input( immutable = true ) @Nonnull final Todo todo )
+  TodoItem( @Input @Nonnull final Todo todo )
   {
     _todo = Objects.requireNonNull( todo );
   }
